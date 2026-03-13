72728914f14e0cd7f544997978c9f8b7250ef838
thefullrob <bartonr@gmail.com>
2026-03-13 18:19:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'common-ground-v3';
+const CACHE_NAME = 'common-ground-v4';
 const ASSETS = [
-  './',
-  './index.html',
-  './app.js?v=20260313',
-  './daily-sets-reviewed.js?v=20260313',
-  './manifest.webmanifest?v=20260313b',
-  './icon-192.png?v=20260313b',
-  './icon-512.png?v=20260313b'
+  '/Common-Ground/',
+  '/Common-Ground/index.html',
+  '/Common-Ground/app.js?v=20260313',
+  '/Common-Ground/daily-sets-reviewed.js?v=20260313',
+  '/Common-Ground/manifest.webmanifest?v=20260313c',
+  '/Common-Ground/icon-192.png?v=20260313c',
+  '/Common-Ground/icon-512.png?v=20260313c'
 ];
 
 self.addEventListener('install', (event) => {
@@ -27,5 +27,6 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
+
 
 
