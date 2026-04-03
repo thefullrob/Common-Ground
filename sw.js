c92e0209717912aba598815e8ebf734ff82763fb
Robert Barton <bartonr@gmail.com>
2026-04-03 10:20:30 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'common-ground-v34';
+const CACHE_NAME = 'common-ground-v36';
 const ASSETS = [
   '/',
   '/index.html',
   '/app.js?v=20260403a',
-  '/daily-sets-reviewed.js?v=20260403b',
-  '/manifest.webmanifest?v=20260403b',
+  '/daily-sets-reviewed.js?v=20260403c',
+  '/manifest.webmanifest?v=20260403d',
   '/icon-192.png?v=20260320a',
   '/icon-512.png?v=20260320a'
 ];
